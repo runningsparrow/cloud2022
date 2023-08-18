259b53f37a9a7cb985bb0b8d67478d3fe8690d2f
runningsparrow <runningsparrow@gmail.com>
2023-08-18 16:54:15 +0800

    sidercar

---
+++ cloudalibaba-sidecar-service8901/src/main/java/com/sparrow/springcloud/alibaba/controller/SidercarController.java
@@ -0,0 +1,44 @@
+package com.sparrow.springcloud.alibaba.controller;
+
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.client.RestTemplate;
+
+import javax.annotation.Resource;
+
+/**
+ * @auther sparrow
+ * @create 2023-08-16 14:13
+ */
+@RestController
+@Slf4j
+public class SidercarController
+{
+    @Resource
+    private RestTemplate restTemplate;
+
+//    @Value("${server.port}")
+//    private String serverPort;
+
+    @GetMapping(value = "/sidercar/flask/health")
+    public String getSiderhealth()
+    {
+        return restTemplate.getForObject("http://localhost:5000/health", String.class);
+    }
+
+
+    @GetMapping(value = "/sidercar/flask/home")
+    public String getflaskhome()
+    {
+        return restTemplate.getForObject("http://localhost:5000/", String.class);
+    }
+
+    @GetMapping(value = "/sidercar/flask/home/test1")
+    public String getflasktest1()
+    {
+        return restTemplate.getForObject("http://localhost:5000/test1", String.class);
+    }
+}
